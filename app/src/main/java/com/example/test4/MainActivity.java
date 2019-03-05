691cb8ec3625df196ae095d56fef90ed056df74a
Simas <simasc10@gmail.com>
2019-03-05 16:02:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.test4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageTest);
+        System.out.print(img.toString());
+        img.setImageResource(R.mipmap.tower);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testMethod(v);
+            }
+        });
+    }
+    public void testMethod(View v)
+    {
+        if(img.getVisibility() == View.GONE)
+            img.setVisibility(View.VISIBLE);
+        else if(img.getVisibility() == View.VISIBLE)
+            img.setVisibility(View.GONE);
+    }
+}
