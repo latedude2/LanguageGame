85ef20bf2d085490f5db9f9b69f1bde5ddc4a5d6
aheged18 <aheged18@student.aau.dk>
2019-03-25 11:14:54 +0100

    new classes

---
+++ app/src/main/java/com/example/test4/PlayerObject.java
@@ -0,0 +1,46 @@
+package com.example.test4;
+
+public class PlayerObject extends GameObject {
+
+    public boolean isMoving;
+
+    public PlayerObject(boolean isMoving){
+        this.isMoving = isMoving;
+    }
+
+    public void Animate(){
+
+    }
+
+    public void MoveLeft(){
+
+    }
+
+    public WorldElement CheckLeftTile(){
+        //return
+    }
+    
+    public void MoveRight() {
+
+    }
+
+    public WorldElement CheckRightTile() {
+        // return
+    }
+
+    public void MoveUp() {
+
+    }
+
+    public WorldElement CheckUpTile() {
+        // return
+    }
+
+    public void MoveDown() {
+
+    }
+
+    public WorldElement CheckDownTile() {
+        // return
+    }
+}
