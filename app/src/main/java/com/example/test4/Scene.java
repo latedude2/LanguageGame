dba61732964abac88363e7727e6d28c9804a8941
Sara886a <43111320+Sara886a@users.noreply.github.com>
2019-04-02 14:26:50 +0200

    fixed last extensions

--- app/src/main/java/com/example/test4/Scene.java
+++ app/src/main/java/com/example/test4/Scene.java
@@ -1,11 +1,6 @@
 package com.example.test4;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
-class Scene extends Object{
+class Scene {
     //private List<Instance> instances = new List<Instance>()
     private boolean open;
     private int xDisplacement;
