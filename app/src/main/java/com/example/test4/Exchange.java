5b9397d6d50ee51abab23ce6b930cc37b7c47cfb
Jokubas126 <jokubas126@users.noreply.github.com>
2019-04-03 14:39:50 +0200

    Update Exchange.java

--- app/src/main/java/com/example/test4/Exchange.java
+++ app/src/main/java/com/example/test4/Exchange.java
@@ -4,30 +4,42 @@ import android.widget.Button;
 import android.widget.TextView;
 
 class Exchange extends Instance{
-    private int numberAnswers = 6;
     private TextView[] text = new TextView[10];
     private Button[] answerButtons = new Button[6]; /** IDs of possible answers **/
-    private Button[] hintButtons; /** buttons for showing the hint images **/
+    private Button[] hintButtons;/** buttons for showing the hint images **/
+    private int[] selectAnswers = new int[6]; /** selected answers during the game play**/
     private int[] correctAnswers = new int[6]; /** answers that are possible to be correct  **/
     private UIElement[] hintImages; /** images shown after pressing the hintButton**/
 
-    public Exchange(TextView[] text, String[] answerText, String[] hintButtons, UIElement[] hintImages, int[] correctAnswers){
-
-        for (int i=0;i<numberAnswers;i++){
-                this.text[i] = text[i];
-                this.answerButtons[i].setText(answerText[i]);
-
+    public Exchange(TextView[] text, String[] answerText, Button[] hintButtons, UIElement[] hintImages, int[] correctAnswers){
+        for (int i=0; i<text.length; i++) {
+            this.text[i] = text[i];
         }
-
+        for (int i=0; i<answerButtons.length; i++){
+            this.answerButtons[i].setText(answerText[i]);
+        }
+        for (int i=0; i<hintButtons.length; i++) {
+            this.hintButtons[i] = hintButtons[i];
+            this.hintImages[i] = hintImages[i];
+        }
+        for (int i=0; i<correctAnswers.length; i++) {
+            this.correctAnswers[i] = correctAnswers[i];
+        }
     }
 
-
-
-    void checkAnswer(){
-
+    void checkAnswer(int[] selectAnswers) {
+        for (int i = 0; i < correctAnswers.length; i++) {
+            if (correctAnswers[i] == selectAnswers[i]) {
+                    /** excepting the answer and saying that you're a good boy **/
+                    System.out.println("good boy");
+                } else {
+                    /** saying that you fucked up **/
+                    System.out.println("you fucked up");
+                }
+            }
     }
 
     void showHint(){
-
+        
     }
 }
