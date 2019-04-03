6a3f8dfd0c66bdfae12587709147b7ec0a452920
Jokubas126 <jokubas126@users.noreply.github.com>
2019-04-03 13:45:40 +0200

    Beginning

--- app/src/main/java/com/example/test4/Exchange.java
+++ app/src/main/java/com/example/test4/Exchange.java
@@ -1,13 +1,27 @@
 package com.example.test4;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 class Exchange extends Instance{
-    private TextView text;
-    private Button  answerButtons;
-    private Button hintButtons;
-    private int correctAnswers;
-    private UIElement hintImages;
+    private int numberAnswers = 6;
+    private TextView[] text = new TextView[10];
+    private Button[] answerButtons = new Button[6]; /** IDs of possible answers **/
+    private Button[] hintButtons; /** buttons for showing the hint images **/
+    private int[] correctAnswers = new int[6]; /** answers that are possible to be correct  **/
+    private UIElement[] hintImages; /** images shown after pressing the hintButton**/
+
+    public Exchange(TextView[] text, String[] answerText, String[] hintButtons, UIElement[] hintImages, int[] correctAnswers){
+
+        for (int i=0;i<numberAnswers;i++){
+                this.text[i] = text[i];
+                this.answerButtons[i].setText(answerText[i]);
+
+        }
+
+    }
+
+
 
     void checkAnswer(){
 
