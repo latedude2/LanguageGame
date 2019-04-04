78f7de43c1f417d097489ff4b9cbe57f6fb6592c
Jokubas126 <jokubas126@users.noreply.github.com>
2019-04-04 15:23:17 +0200

    fuck that shit in Exchange

--- app/src/main/java/com/example/test4/Exchange.java
+++ app/src/main/java/com/example/test4/Exchange.java
@@ -1,13 +1,17 @@
 package com.example.test4;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 class Exchange extends Instance {
-    private TextView[] text = new TextView[10];
+    private Button[] answerPos;
     private Button[] answerButtons = new Button[6]; //IDs of possible answers
     private Button[] hintButtons; //buttons for showing the hint images
+    private String sentence = "Okay, " + answerButtons[0].getText() + "kan " + answerButtons[1].getText() + "finde dem?";
+    private TextView[] text = new TextView[5];
     private int[] selectAnswers = new int[6]; //selected answers during the game play
     private int[] correctAnswers = new int[6]; //answers that are possible to be correct
     private GameObject[] UIObjects; //images shown after pressing the hintButton
@@ -28,17 +32,31 @@ class Exchange extends Instance {
         }
     }
 
-    void checkAnswer(int[] selectAnswers) {
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatActivity.onCreate(savedInstanceState);
+        AppCompatActivity.setContentView(R.layout.activity_main);
+    }*/
+
+    /*public void clickAnswer(CharSequence btn){
+        for (int i = 0; i < answerPos.length; i++) {
+            if (answerPos[i].getAccessibilityClassName() != null){
+                answerPos[i].getAccessibilityClassName() = btn;
+            }
+        }
+    }*/
+
+    /*void checkAnswer(Button[] answerPos) {
         for (int i = 0; i < correctAnswers.length; i++) {
-            if (correctAnswers[i] == selectAnswers[i]) {
-                /** excepting the answer and saying that you're a good boy **/
+            if (correctAnswers[i] == answerPos[i]) {
+                // excepting the answer and saying that you're a good boy
                 System.out.println("good boy");
             } else {
-                /** saying that you fucked up **/
+                // saying that you fucked up
                 System.out.println("you fucked up");
             }
         }
-    }
+    }*/
 
     /*public int checkHint(){
         int x; //the index of an image
@@ -47,7 +65,7 @@ class Exchange extends Instance {
     }*/
 
 
-    void showHint(int x) {
-        UIObjects[x] = 
-    }
+    /*void showHint(int x) {
+        UIObjects[x] =
+    }*/
 }
