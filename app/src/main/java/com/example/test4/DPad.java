9bba8dff7be556819ef5b2b77697347c974a27f7
Sara886a <43111320+Sara886a@users.noreply.github.com>
2019-04-24 13:33:06 +0200

    added the current code for movement in overworld

--- app/src/main/java/com/example/test4/DPad.java
+++ app/src/main/java/com/example/test4/DPad.java
@@ -6,13 +6,44 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 class DPad {
-    
 
-    float moveX =  MainActivity.background.getX();
+    //this should be in the main activity for the overworld
+    //background = (ImageView) findViewById(R.id.background);
+
+    int moveDist = 50;
+
+    /*
+    float moveX =  someActivity.background.getX();
+    float moveY =  someActivity.background.getY();
 
     public void moveUp (){
+        moveY += moveDist;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(background, "y", background.getY(), moveY);
+            animation.setDuration(200);
+            animation.start();
+    }
+
+    public void moveDown (){
+        moveY -= moveDist;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(background, "y", background.getY(), moveY);
+            animation.setDuration(200);
+            animation.start();
 
     }
 
+    public void moveLeft (){
+        moveX += moveDist;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(background, "X", background.getX(), moveX);
+            animation.setDuration(200);
+            animation.start();
+    }
+
+    public void moveRight (){
+        moveX -= moveDist;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(background, "X", background.getX(), moveX);
+            animation.setDuration(200);
+            animation.start();
+    }
+    */
 
 }
