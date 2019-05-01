d9de867c91d241119f0c878188238bbffc8e53fb
Aba <mhadden-a@hotmail.com>
2019-05-01 15:05:51 +0200

    movement working

--- app/src/main/java/com/example/test4/MainActivity.java
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -1,26 +1,13 @@
 package com.example.test4;
 
 import android.app.Activity;
-import android.content.Context;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class MainActivity extends Activity {
 
@@ -33,7 +20,6 @@ public class MainActivity extends Activity {
     private TextView dialoguetext;
     private ImageView hintImage;
 
-    private String question;
     int i = 1; //index which counts which exchange it is currently
 
     @Override
@@ -42,25 +28,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dialoguetext = (TextView) findViewById(R.id.dialogue_text);
-        hintImage = (ImageView) findViewById(R.id.hint_img);
-
-        //TO BE PUT INTO OnClickListener()
-        String index = Integer.toString(i); //use if it complains about using integer in the String in the following line
-        String id = "exchange" + index; //creates a String name of the file to use in the following line
-        int idOfFile = getResources().getIdentifier(id,"raw", getPackageName());
-        InputStream inputStream = this.getResources().openRawResource(idOfFile);
-        FileRead file = new FileRead(i, inputStream); //creates the file object for all the Strings to be created there
-        file.read();
-        //creates exchange object which consists of all the Strings to be put in that one created exchange
-        Exchange exchange = new Exchange(file.getAnswerText(), file.getQuestionText(), file.getHintText(), file.getAllAnswers(), file.getGapText(), file.getCorrectAnswers());
-
-        dialoguetext.setText(exchange.clickHint());
-        dialoguetext.setMovementMethod(LinkMovementMethod.getInstance());
-
         loadImage();
-
-
+        loadExchange();
     }
 
     public void loadImage(){
@@ -71,6 +40,20 @@ public class MainActivity extends Activity {
         int idOfImage = getResources().getIdentifier("map", "drawable", getPackageName());
         dPad.showImage(backgroundMap, idOfImage);
         conversationBack.setImageResource(R.drawable.background);
+    }
+    public void loadExchange(){
+        //TO BE PUT INTO OnClickListener()
+        String index = Integer.toString(i); //use if it complains about using integer in the String in the following line
+        String id = "exchange" + index; //creates a String name of the file to use in the following line
+        int idOfFile = getResources().getIdentifier(id,"raw", getPackageName());
+        InputStream inputStream = this.getResources().openRawResource(idOfFile);
+        FileRead file = new FileRead(i, inputStream); //creates the file object for all the Strings to be created there
+        file.read();
+        //creates exchange object which consists of all the Strings to be put in that one created exchange
+        Exchange exchange = new Exchange(file.getAnswerText(), file.getQuestionText(), file.getHintText(), file.getAllAnswers(), file.getGapText(), file.getCorrectAnswers());
+        dialoguetext = (TextView) findViewById(R.id.dialogue_text);
+        dialoguetext.setText(exchange.checkHint());
+        dialoguetext.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void move_characterUp (View v){
@@ -89,8 +72,8 @@ public class MainActivity extends Activity {
         dPad.moveRight();
     }
 
-    public ImageView getHintImage() {
-        //hintImage.setImageResource(R.drawable.background);
+    public ImageView getHintImage(){
+        hintImage = (ImageView) findViewById(R.id.hint_img);
         return hintImage;
     }
 }
