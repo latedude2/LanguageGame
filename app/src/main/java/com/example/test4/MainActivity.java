8990ec2d70c6adceb3e4d61efd2cd5304129efc4
Simas <simasc10@gmail.com>
2019-05-07 11:35:34 +0200

    Fixed merge

--- app/src/main/java/com/example/test4/MainActivity.java
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity {
         FileRead file = new FileRead(i, inputStream); //creates the file object for all the Strings to be created there
         file.read();
         //creates exchange object which consists of all the Strings to be put in that one created exchange
-        Exchange exchange = new Exchange(file.getAnswerText(), file.getQuestionText(), file.getAllAnswers(), file.getGapText(), file.getCorrectAnswers(), this);
+        Exchange exchange = new Exchange(file.getAnswerText(), file.getQuestionText(), file.getAllAnswers(), file.getCorrectAnswers(), this);
         dialoguetext = findViewById(R.id.dialogue_text);
         dialoguetext.setText(exchange.checkHint());
         dialoguetext.setMovementMethod(LinkMovementMethod.getInstance());
