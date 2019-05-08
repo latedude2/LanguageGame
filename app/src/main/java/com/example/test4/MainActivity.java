a39266bdbfb4f4417d7e0ecb2f87fa203d4e4810
Aba <mhadden-a@hotmail.com>
2019-05-08 15:05:09 +0200

    fixing merging issues

--- app/src/main/java/com/example/test4/MainActivity.java
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends Activity {
 
     private  ConversationController conversationController;
 
-    private TextView[] answerButtonsTextView = new TextView[6];
+    //private TextView[] answerButtonsTextView = new TextView[6];
     private ImageView speaker_button;
     private ImageView submit_button;
 
@@ -95,7 +95,7 @@ public class MainActivity extends Activity {
         int idOfMap = getResources().getIdentifier("map", "drawable", getPackageName());
         backgroundMap.setImageResource(idOfMap);
 
-        idOfImage = getResources().getIdentifier("background_convo", "drawable", getPackageName());
+        int idOfImage = getResources().getIdentifier("background_convo", "drawable", getPackageName());
         conversationLayout.setImageResource(idOfImage);
 
         submit_button = findViewById(R.id.submit_button);
@@ -106,187 +106,181 @@ public class MainActivity extends Activity {
     }
 
     //loads all the elements of the exchange view
-    public void loadExchange()
-    {
+    public void loadExchange() {
         String index = Integer.toString(exchangeIndex); //use if it complains about using integer in the String in the following line
         String id = "exchange" + index; //creates a String name of the file to use in the following line
-        int idOfFile = getResources().getIdentifier(id,"raw", getPackageName());
+        int idOfFile = getResources().getIdentifier(id, "raw", getPackageName());
         InputStream inputStream = this.getResources().openRawResource(idOfFile);
         file = new FileRead(inputStream); //creates the file object for all the Strings to be created there
         file.read();
         //creates exchange object which consists of all the Strings to be put in that one created exchange
 
         exchange = new Exchange(file.getAnswerText(), file.getQuestionText(), file.getAllAnswers(), this);
-        dialoguetext = findViewById(R.id.dialogue_text);
-        dialoguetext.setText(exchange.checkHint());
-        dialoguetext.setMovementMethod(LinkMovementMethod.getInstance());
+        dialogueText = findViewById(R.id.dialogue_text);
+        dialogueText.setText(exchange.checkHint());
+        dialogueText.setMovementMethod(LinkMovementMethod.getInstance());
 
         answerText = findViewById(R.id.answer_text);
         answerText.setText(exchange.checkGap());
 
         //exchange.takeAnswers(this.i);
-        for (int j = 0; j < answerButtonsTextView.length; j++){
- 
-        for (int i = 0; i < answerButtonsTextView.length; i++){
-            String number = Integer.toString(i);
-            String viewText = "answer_button_text_" + number;
-            int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
-            answerButtonsTextView[i] = findViewById(textViewId);
-            answerButtonsTextView[i].setText(exchange.takeAnswers(i));
-        }
-
-        onSoundViewClick();
-        exchange.resetSelectedAnswers();
-
-    }
-
-
-
-    public void loadConverstationCharacterImage(){
-        ImageView imageView = findViewById(R.id.npc_dialogue_view);
-        imageView.setImageResource(R.drawable.big_baker);
-    }
-
-    public void move_characterUp (View v){
-        dPad.moveUp();
-    }
-
-    public void move_characterDown (View v){
-        dPad.moveDown();
-    }
-
-    public void move_characterLeft (View v){
-        dPad.moveLeft();
-    }
-
-    public void move_characterRight (View v){
-        dPad.moveRight();
-    }
-
-    public ImageView getNpcDialogueView() {
-        npcDialogueView = findViewById(R.id.npc_dialogue_view);
-        return npcDialogueView;
-    }
-
-    public ImageView getHintImage(){
-        hintImage = findViewById(R.id.hint_img);
-        return hintImage;
-    }
-
-    public TextView getDialogueText() {
-        dialogueText = findViewById(R.id.dialogue_text);
-        return dialogueText;
-    }
-
-    public TextView getAnswerText() {
-        answerText = findViewById(R.id.answer_text);
-        return answerText;
-    }
-
-    public ImageView getConversationLayout() {
-        conversationLayout = findViewById(R.id.conversation_layout);
-        return conversationLayout;
-    }
-
-    public ImageView getAnswerTextField() {
-        answerTextField = findViewById(R.id.answer_text_field);
-        return answerTextField;
-    }
-
-    public ImageView getSpeakerButton() {
-        speakerButton = findViewById(R.id.speaker_button);
-        return speakerButton;
-    }
-
-    public ImageView getSubmitButton() {
-        submitButton = findViewById(R.id.submit_button);
-        return submitButton;
-    }
-
-    public TextView[] getAnswerButtonsTextView() {
         for (int j = 0; j < answerButtonsTextView.length; j++) {
-            String number = Integer.toString(j);
-            String viewText = "answer_button_text_" + number;
-            int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
-            answerButtonsTextView[j] = findViewById(textViewId);
+
+            for (int i = 0; i < answerButtonsTextView.length; i++) {
+                String number = Integer.toString(i);
+                String viewText = "answer_button_text_" + number;
+                int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
+                answerButtonsTextView[i] = findViewById(textViewId);
+                answerButtonsTextView[i].setText(exchange.takeAnswers(i));
+            }
+
+            onSoundViewClick();
+            exchange.resetSelectedAnswers();
+
         }
-        return answerButtonsTextView;
-    }
 
-    public ImageView[] getAnswerButtons() {
-        for (int j = 0; j < answerButtons.length; j++) {
-            String number = Integer.toString(j);
-            String imageView = "answer_button_" + number;
-            int imageViewId = getResources().getIdentifier(imageView, "id", getPackageName());
-            answerButtons[j] = findViewById(imageViewId);
+    }
+        public void loadConverstationCharacterImage () {
+            ImageView imageView = findViewById(R.id.npc_dialogue_view);
+            imageView.setImageResource(R.drawable.big_baker);
         }
-        return answerButtons;
-    }
 
-    public void answerClick(View view){
-        ImageView answer = (ImageView) view;
-        int id = answer.getId();
-        String answerTextName = answer.getResources().getResourceName(id);
-        char takeNum = answerTextName.charAt(answerTextName.length()-1);
-        String textViewName = "answer_button_text_" + takeNum;
-        int idOfTextView = getResources().getIdentifier(textViewName, "id", getPackageName());
-        TextView answerTextView = findViewById(idOfTextView);
-        String answerTextToPut = answerTextView.getText().toString();
-        TextView answerField = findViewById(R.id.answer_text);
-        answerField.setMovementMethod(LinkMovementMethod.getInstance());        //Make the text view clickable. This enables us to add ClickableSpan to this Text View
+        public void move_characterUp (View v){
+            dPad.moveUp();
+        }
+
+        public void move_characterDown (View v){
+            dPad.moveDown();
+        }
+
+        public void move_characterLeft (View v){
+            dPad.moveLeft();
+        }
+
+        public void move_characterRight (View v){
+            dPad.moveRight();
+        }
+
+        public ImageView getNpcDialogueView () {
+            npcDialogueView = findViewById(R.id.npc_dialogue_view);
+            return npcDialogueView;
+        }
+
+        public ImageView getHintImage () {
+            hintImage = findViewById(R.id.hint_img);
+            return hintImage;
+        }
+
+        public TextView getDialogueText () {
+            dialogueText = findViewById(R.id.dialogue_text);
+            return dialogueText;
+        }
+
+        public TextView getAnswerText () {
+            answerText = findViewById(R.id.answer_text);
+            return answerText;
+        }
+
+        public ImageView getConversationLayout () {
+            conversationLayout = findViewById(R.id.conversation_layout);
+            return conversationLayout;
+        }
+
+        public ImageView getAnswerTextField () {
+            answerTextField = findViewById(R.id.answer_text_field);
+            return answerTextField;
+        }
+
+        public ImageView getSpeakerButton () {
+            speakerButton = findViewById(R.id.speaker_button);
+            return speakerButton;
+        }
+
+        public ImageView getSubmitButton () {
+            submitButton = findViewById(R.id.submit_button);
+            return submitButton;
+        }
+
+        public TextView[] getAnswerButtonsTextView () {
+            for (int j = 0; j < answerButtonsTextView.length; j++) {
+                String number = Integer.toString(j);
+                String viewText = "answer_button_text_" + number;
+                int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
+                answerButtonsTextView[j] = findViewById(textViewId);
+            }
+            return answerButtonsTextView;
+        }
+
+        public ImageView[] getAnswerButtons () {
+            for (int j = 0; j < answerButtons.length; j++) {
+                String number = Integer.toString(j);
+                String imageView = "answer_button_" + number;
+                int imageViewId = getResources().getIdentifier(imageView, "id", getPackageName());
+                answerButtons[j] = findViewById(imageViewId);
+            }
+            return answerButtons;
+        }
+
+        public void answerClick (View view){
+            ImageView answer = (ImageView) view;
+            int id = answer.getId();
+            String answerTextName = answer.getResources().getResourceName(id);
+            char takeNum = answerTextName.charAt(answerTextName.length() - 1);
+            String textViewName = "answer_button_text_" + takeNum;
+            int idOfTextView = getResources().getIdentifier(textViewName, "id", getPackageName());
+            TextView answerTextView = findViewById(idOfTextView);
+            String answerTextToPut = answerTextView.getText().toString();
+            TextView answerField = findViewById(R.id.answer_text);
+            answerField.setMovementMethod(LinkMovementMethod.getInstance());        //Make the text view clickable. This enables us to add ClickableSpan to this Text View
 
 
+            fullAnswer = exchange.getUsersAnswerUnchanged();
+            answerSlotCount = 0;
+            String tempString = fullAnswer;
+            while ((tempString).contains("____")) {
+                tempString = tempString.substring(tempString.indexOf("____") + 4);
+                fullAnswer = fullAnswer.replaceFirst("____", "#" + answerSlotCount);
+                answerSlotCount++;
+            }
+            //System.out.println(fullAnswer);
 
-        fullAnswer = exchange.getUsersAnswerUnchanged();
-        answerSlotCount = 0;
-        String tempString = fullAnswer;
-        while((tempString).contains("____"))
+            if (answerField.getText().toString().contains("____"))   //If there is a slot to put the new word in
+            {
+                putWordInSlot(answerTextToPut);
+                SpannableString spanString = buildSpannableString(selectedAnswers, fullAnswer, answerSlotCount);
+                answerField.setText(spanString);
+            }
+        }
+        //void putWordInSlot(String ans)
+
+        public void answerClick (View view)
         {
-            tempString = tempString.substring(tempString.indexOf("____") + 4);
-            fullAnswer = fullAnswer.replaceFirst("____", "#" + answerSlotCount);
-            answerSlotCount++;
-        }
-        //System.out.println(fullAnswer);
+            exchange.addAnswer(view);
 
-        if(answerField.getText().toString().contains("____"))   //If there is a slot to put the new word in
+        }
+
+
+        public void onSubmitClick (View view)
         {
-            putWordInSlot(answerTextToPut);
-            SpannableString spanString = buildSpannableString(selectedAnswers, fullAnswer, answerSlotCount);
-            answerField.setText(spanString);
+            exchange.submitAnswer(view);
+
+
+        }
+
+        public void onSoundViewClick () {
+            String audioFileName = "sentence" + exchangeIndex;
+            final int idOfAudioFile = getResources().getIdentifier(audioFileName, "raw", getPackageName());
+            exchange.sentencePlay(speaker_button, idOfAudioFile);
+        }
+
+
+        //creates the 2D array mapTiles, which holds the structure of the map
+        public void loadMapStructure () {
+            String idName = "map_structure"; //creates a String name of the file to use in the following line
+            int idOfFile = getResources().getIdentifier(idName, "raw", getPackageName());
+            InputStream inputStream = this.getResources().openRawResource(idOfFile);
+            FileRead fileStructure = new FileRead(inputStream); //creates the file object for all the Strings to be created there
+            mapTiles = fileStructure.readStructureChars(); //reads all of the chars in the structure and adds them to the 2D array
         }
     }
-    //void putWordInSlot(String ans)
-
-    public void answerClick(View view)
-    {
-        exchange.addAnswer(view);
-
-    }
-
-
-    public void onSubmitClick(View view)
-    {
-        exchange.submitAnswer(view);
-
-
-    }
-
-    public void onSoundViewClick()
-    {
-        String audioFileName = "sentence" + exchangeIndex;
-        final int idOfAudioFile = getResources().getIdentifier(audioFileName, "raw", getPackageName());
-        exchange.sentencePlay(speaker_button, idOfAudioFile);
-    }
-
-
-
-    //creates the 2D array mapTiles, which holds the structure of the map
-    public void loadMapStructure(){
-        String idName = "map_structure"; //creates a String name of the file to use in the following line
-        int idOfFile = getResources().getIdentifier(idName,"raw", getPackageName());
-        InputStream inputStream = this.getResources().openRawResource(idOfFile);
-        FileRead fileStructure = new FileRead(inputStream); //creates the file object for all the Strings to be created there
-        mapTiles = fileStructure.readStructureChars(); //reads all of the chars in the structure and adds them to the 2D array
-    }
-
 }
