c1a23aa39f7574f1b28f58421b0a38fb13340b96
Aba <mhadden-a@hotmail.com>
2019-05-08 14:46:23 +0200

    exit button working on conversationcontroller class

--- app/src/main/java/com/example/test4/ConversationController.java
+++ app/src/main/java/com/example/test4/ConversationController.java
@@ -2,6 +2,7 @@ package com.example.test4;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ConversationController extends Instance{
 
@@ -10,16 +11,106 @@ public class ConversationController extends Instance{
     //private ImageView backButton;
     private int currentExchangeID;
     private OurButton continueButton;
-    MainActivity mainActivity = new MainActivity();
-    ConversationController(){}
+
+    private int sizeSix = 6;
+
+    private ImageView npcDialogueView;      //Image of the character you are having the conversation with
+    private ImageView hintImage;            //Image view to show the hint of a word
+    private TextView dialogueText;          //Text view to hold the text of the NPC
+    private TextView answerText;            //Text view to hold the text of the user
+    private ImageView conversationLayout;     //Image view to show the background of a conversation
+    private ImageView answerTextField;      //Image where the answer is displayed on
+    private ImageView speakerButton;        //Clickable image to play the sound
+    private ImageView submitButton;        //Clickable image to submit the answer
+    private ImageView exitButton;           //Clickable image to exit the conversation
+    private TextView[] answerButtonsTextView = new TextView[sizeSix]; //List of the answer choices displayed
+    private ImageView[] answerButtons = new ImageView[sizeSix];   //List of Images where the answer choices are displayed on
+
+    ConversationController(MainActivity mainActivity){
+        this.npcDialogueView = mainActivity.findViewById(R.id.npc_dialogue_view);
+        this.hintImage = mainActivity.findViewById(R.id.hint_img);
+        this.dialogueText = mainActivity.findViewById(R.id.dialogue_text);
+        this.answerText = mainActivity.findViewById(R.id.answer_text);
+        this.conversationLayout = mainActivity.findViewById(R.id.conversation_layout);
+        this.answerTextField = mainActivity.findViewById(R.id.answer_text_field);
+        this.speakerButton = mainActivity.findViewById(R.id.speaker_button);
+        this.submitButton = mainActivity.findViewById(R.id.submit_button);
+        this.exitButton = mainActivity.findViewById(R.id.exit_button);
+        for (int j = 0; j < sizeSix; j++) {
+            String number = Integer.toString(j);
+            String viewText = "answer_button_text_" + number;
+            String imageView = "answer_button_" + number;
+            int textViewId = mainActivity.getResources().getIdentifier(viewText, "id", mainActivity.getPackageName());
+            int imageViewId= mainActivity.getResources().getIdentifier(imageView, "id", mainActivity.getPackageName());
+            this.answerButtonsTextView[j] = mainActivity.findViewById(textViewId);
+            this.answerButtons[j]=mainActivity.findViewById(imageViewId);
+        }
+
+    }
 
     public void showExchange(){
 
     }
 
     public void exitConversation(){
-        mainActivity.getHintImage().setVisibility(View.GONE);
-        mainActivity.getAnswerText().setVisibility(View.GONE);
-        mainActivity.getDialogueText().setVisibility(View.GONE);
+        for (int j = 0; j < answerButtonsTextView.length ; j++) {
+            getAnswerButtons()[j].setVisibility(View.GONE);
+            getAnswerButtonsTextView()[j].setVisibility(View.GONE);
+        }
+        getNpcDialogueView().setVisibility(View.GONE);
+        getHintImage().setVisibility(View.GONE);
+        getDialogueText().setVisibility(View.GONE);
+        getAnswerText().setVisibility(View.GONE);
+        getConversationLayout().setVisibility(View.GONE);
+        getAnswerTextField().setVisibility(View.GONE);
+        getSpeakerButton().setVisibility(View.GONE);
+        getSubmitButton().setVisibility(View.GONE);
+        getExitButton().setVisibility(View.GONE);
+
+
+    }
+
+    public ImageView getNpcDialogueView() {
+        return npcDialogueView;
+    }
+
+    public ImageView getHintImage() {
+        return hintImage;
+    }
+
+    public TextView getDialogueText() {
+        return dialogueText;
+    }
+
+    public TextView getAnswerText() {
+        return answerText;
+    }
+
+    public ImageView getConversationLayout() {
+        return conversationLayout;
+    }
+
+    public ImageView getAnswerTextField() {
+        return answerTextField;
+    }
+
+    public ImageView getSpeakerButton() {
+        return speakerButton;
+    }
+
+    public ImageView getSubmitButton() {
+        return submitButton;
+    }
+
+    public ImageView getExitButton() {
+        return exitButton;
+    }
+
+    public TextView[] getAnswerButtonsTextView() {
+        return answerButtonsTextView;
+    }
+
+    public ImageView[] getAnswerButtons() {
+        return answerButtons;
     }
 }
--- app/src/main/java/com/example/test4/MainActivity.java
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -8,6 +8,7 @@ import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,18 +17,29 @@ import java.io.InputStream;
 public class MainActivity extends Activity {
 
     private ImageView backgroundMap;        //Image view to show the map
-    private ImageView conversationBack;     //Image view to show the background of a conversation
+
     private DPad dPad;                      //Controls for walking
-    private TextView answerText;            //Text view to hold the text of the user
-    private TextView dialogueText;          //Text view to hold the text of the NPC
+
+
+
+
+    //All conversation elements
+    private ImageView npcDialogueView;      //Image of the character you are having the conversation with
     private ImageView hintImage;            //Image view to show the hint of a word
+    private TextView dialogueText;          //Text view to hold the text of the NPC
+    private TextView answerText;            //Text view to hold the text of the user
+    private ImageView conversationLayout;     //Image view to show the background of a conversation
+    private ImageView answerTextField;      //Image where the answer is displayed on
+    private ImageView speakerButton;        //Clickable image to play the sound
+    private ImageView submitButton;        //Clickable image to submit the answer
+    private TextView[] answerButtonsTextView = new TextView[6]; //List of the answer choices displayed
+    private ImageView[] answerButtons = new ImageView[6];   //List of Images where the answer choices are displayed on
 
-    private ImageView exiteButton;
-
+    //All world view elements
 
     private Exchange exchange;
     private  ConversationController conversationController;
-    private TextView[] answerButtonsTextView = new TextView[6];
+
 
 
     private String fullAnswer;
@@ -64,11 +76,12 @@ public class MainActivity extends Activity {
     }
 
     public void beGone(View view){
-        conversationController = new ConversationController();
+        conversationController = new ConversationController(this);
         conversationController.exitConversation();
-        //getHintImage().setVisibility(View.GONE);
-        //getAnswerText().setVisibility(View.GONE);
-        //getDialogueText().setVisibility(View.GONE);
+    }
+
+    public void beCome(){
+
     }
 
     public void loadLayoutImage(){
@@ -76,13 +89,13 @@ public class MainActivity extends Activity {
         backgroundMap = findViewById(R.id.world_view);
         dPad = new DPad(backgroundMap, this);
 
-        conversationBack = findViewById(R.id.conversation_view);
+        conversationLayout = findViewById(R.id.conversation_layout);
 
         idOfImage = getResources().getIdentifier("map", "drawable", getPackageName());
         backgroundMap.setImageResource(idOfImage);
 
         idOfImage = getResources().getIdentifier("background_convo", "drawable", getPackageName());
-        conversationBack.setImageResource(idOfImage);
+        conversationLayout.setImageResource(idOfImage);
 
         ImageView submit_button = findViewById(R.id.submit_button);
         submit_button.setImageResource(R.drawable.proceed_button);
@@ -110,13 +123,9 @@ public class MainActivity extends Activity {
 
         answerButtonsTextView = new TextView[6];
 
-        exchange.takeAnswers(this.i);
+        //exchange.takeAnswers(this.i);
         for (int j = 0; j < answerButtonsTextView.length; j++){
-            String number = Integer.toString(i);
-            String viewText = "answer_button_text_" + number;
-            int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
-            answerButtonsTextView[j] = findViewById(textViewId);
-            answerButtonsTextView[j].setText(exchange.takeAnswers(this.i));
+            getAnswerButtonsTextView()[j].setText(exchange.takeAnswers(j));
         }
 
         //Reseting selected answers
@@ -126,6 +135,8 @@ public class MainActivity extends Activity {
         }
 
     }
+
+
 
     public void loadConverstationCharacterImage(){
         ImageView imageView = findViewById(R.id.npc_dialogue_view);
@@ -148,19 +159,14 @@ public class MainActivity extends Activity {
         dPad.moveRight();
     }
 
+    public ImageView getNpcDialogueView() {
+        npcDialogueView = findViewById(R.id.npc_dialogue_view);
+        return npcDialogueView;
+    }
 
     public ImageView getHintImage(){
         hintImage = findViewById(R.id.hint_img);
         return hintImage;
-    }
-    public TextView[] getAnswerButtonsTextView() {
-        for (int j = 0; j < answerButtonsTextView.length; j++) {
-            String number = Integer.toString(j);
-            String viewText = "answer_button_text_" + number;
-            int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
-            answerButtonsTextView[j] = findViewById(textViewId);
-        }
-        return answerButtonsTextView;
     }
 
     public TextView getDialogueText() {
@@ -173,9 +179,44 @@ public class MainActivity extends Activity {
         return answerText;
     }
 
-    public ImageView getExiteButton() {
-        exiteButton = findViewById(R.id.exit_menu_button);
-        return exiteButton;
+    public ImageView getConversationLayout() {
+        conversationLayout = findViewById(R.id.conversation_layout);
+        return conversationLayout;
+    }
+
+    public ImageView getAnswerTextField() {
+        answerTextField = findViewById(R.id.answer_text_field);
+        return answerTextField;
+    }
+
+    public ImageView getSpeakerButton() {
+        speakerButton = findViewById(R.id.speaker_button);
+        return speakerButton;
+    }
+
+    public ImageView getSubmitButton() {
+        submitButton = findViewById(R.id.submit_button);
+        return submitButton;
+    }
+
+    public TextView[] getAnswerButtonsTextView() {
+        for (int j = 0; j < answerButtonsTextView.length; j++) {
+            String number = Integer.toString(j);
+            String viewText = "answer_button_text_" + number;
+            int textViewId = getResources().getIdentifier(viewText, "id", getPackageName());
+            answerButtonsTextView[j] = findViewById(textViewId);
+        }
+        return answerButtonsTextView;
+    }
+
+    public ImageView[] getAnswerButtons() {
+        for (int j = 0; j < answerButtons.length; j++) {
+            String number = Integer.toString(j);
+            String imageView = "answer_button_" + number;
+            int imageViewId = getResources().getIdentifier(imageView, "id", getPackageName());
+            answerButtons[j] = findViewById(imageViewId);
+        }
+        return answerButtons;
     }
 
     public void answerClick(View view){
