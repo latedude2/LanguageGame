f92e475fd5d470539cfe8c53f02415f738073e9a
Simas <simasc10@gmail.com>
2019-05-14 14:08:09 +0200

    Fixed Kviterring crash caused by capitalized letters

--- app/src/main/java/com/example/test4/Exchange.java
+++ app/src/main/java/com/example/test4/Exchange.java
@@ -213,6 +213,7 @@ class Exchange extends Instance {
         word = word.replaceAll("Æ", "Ae");
         word = word.replaceAll("Ø", "O");
         word = word.replaceAll("Å", "Aa");
+        word = word.toLowerCase();
         return word;
     }
     //-----------------
